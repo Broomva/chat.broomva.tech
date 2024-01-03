d68aa746a4cbaf4768c7fa36c0dcbc1068cd0e34
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-01-03 13:09:36 -0500

    Added auth options

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from typing import Dict, Optional
 
 import chainlit as cl
 from chainlit.input_widget import Select, Slider, Switch
+from chainlit.playground.config import add_llm_provider
+from chainlit.playground.providers.langchain import LangchainGenericProvider
 # from chainlit import user_session
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import HuggingFaceHub
 from langchain.prompts.chat import (AIMessagePromptTemplate,
                                     ChatPromptTemplate,
                                     HumanMessagePromptTemplate)
@@ -18,50 +21,82 @@ embeddings = OpenAIEmbeddings()
 vector_store = FAISS.load_local("docs.faiss", embeddings)
 
 
-# @cl.oauth_callback
-# def oauth_callback(
-#     provider_id: str,
-#     token: str,
-#     raw_user_data: Dict[str, str],
-#     default_app_user: cl.AppUser,
-# ) -> Optional[cl.AppUser]:
-#     # set AppUser tags as regular_user
-#     match default_app_user.username:
-#         case "Broomva":
-#             default_app_user.tags = ["admin_user"]
-#             default_app_user.role = "ADMIN"
-#         case _:
-#             default_app_user.tags = ["regular_user"]
-#             default_app_user.role = "USER"
-#     print(default_app_user)
-#     return default_app_user
+@cl.oauth_callback
+def oauth_callback(
+    provider_id: str,
+    token: str,
+    raw_user_data: Dict[str, str],
+    default_app_user: cl.AppUser,
+) -> Optional[cl.AppUser]:
+    # set AppUser tags as regular_user
+    match default_app_user.username:
+        case "Broomva":
+            default_app_user.tags = ["admin_user"]
+            default_app_user.role = "ADMIN"
+        case _:
+            default_app_user.tags = ["regular_user"]
+            default_app_user.role = "USER"
+    print(default_app_user)
+    return default_app_user
+
+@cl.header_auth_callback
+def header_auth_callback(headers) -> Optional[cl.AppUser]:
+    # Verify the signature of a token in the header (ex: jwt token)
+    # or check that the value is matching a row from your database
+    print(headers)
+    if headers.get("cookie") == "ajs_user_id=5011e946-0d0d-5bd4-a293-65742db98d3d; ajs_anonymous_id=67d2569d-3f50-48f3-beaf-b756286276d9":
+        return cl.AppUser(username="Broomva", role="ADMIN", provider="header")
+    else:
+        return None
 
 
-# @cl.set_chat_profiles
-# async def chat_profile(current_user: cl.AppUser):
-#     if "admin_user" not in current_user.tags:
-#         # Default to 3.5 when not admin
-#         return [
-#             cl.ChatProfile(
-#                 name="GPT-3.5",
-#                 markdown_description="The underlying LLM model is **GPT-3.5**.",
-#                 icon="https://picsum.photos/200",
-#             )
-#         ]
+@cl.password_auth_callback
+def auth_callback(username: str = 'guest', password: str = 'guest') -> Optional[cl.AppUser]:
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    import hashlib
 
-#     return [
-#         cl.ChatProfile(
-#             name="GPT-3.5",
-#             markdown_description="The underlying LLM model is **GPT-3.5**.",
-#             icon="https://picsum.photos/200",
-#         ),
-#         cl.ChatProfile(
-#             name="GPT-4",
-#             markdown_description="The underlying LLM model is **GPT-4**.",
-#             icon="https://picsum.photos/250",
-#         ),
-#     ]
+    # Create a new sha256 hash object
+    hash_object = hashlib.sha256()
 
+    # Hash the password
+    hash_object.update(password.encode())
+
+    # Get the hexadecimal representation of the hash
+    hashed_password = hash_object.hexdigest()
+    
+    if (username, hashed_password) == ("broomva", "b68cacbadaee450b8a8ce2dd44842f1de03ee9993ad97b5e99dea64ef93960ba"):
+        return cl.AppUser(username="Broomva", role="ADMIN", provider="credentials")
+    elif (username, password) == ("guest", "guest"):
+        return cl.AppUser(username="Guest", role="USER", provider="credentials")
+    else:
+        return None
+
+@cl.set_chat_profiles
+async def chat_profile(current_user: cl.AppUser):
+    if "ADMIN" not in current_user.role:
+        # Default to 3.5 when not admin
+        return [
+            cl.ChatProfile(
+                name="Broomva Book Agent",
+                markdown_description="The underlying LLM model is **GPT-3.5**.",
+                # icon="https://picsum.photos/200",
+            ),
+        ]
+
+    return [
+        cl.ChatProfile(
+            name="Broomva Book Agent Lite",
+            markdown_description="The underlying LLM model is **GPT-3.5**.",
+            # icon="https://picsum.photos/200",
+        ),
+        cl.ChatProfile(
+            name="Broomva Book Agent Turbo",
+            markdown_description="The underlying LLM model is **GPT-4 Turbo**.",
+            # icon="https://picsum.photos/250",
+        ),
+    ]
+    
 
 @cl.on_settings_update
 async def setup_agent(settings):
@@ -90,7 +125,7 @@ async def init():
             Slider(
                 id="k",
                 label="RAG - Retrieved Documents",
-                initial=3,
+                initial=5,
                 min=1,
                 max=20,
                 step=1,
@@ -98,28 +133,32 @@ async def init():
         ]
     ).send()
     
+    chat_profile = cl.user_session.get("chat_profile")
     
-    # print(settings)
-    # app_user = cl.user_session.get("user")
-    # chat_profile = cl.user_session.get("chat_profile")
-    # await cl.Message(
-    #     content=f"🪼 Starting chat with {app_user.username} using the {chat_profile} chat profile"
-    # ).send()
+    if chat_profile == "Broomva Book Agent Lite":
+        settings['model'] = "gpt-3.5-turbo"
+    elif chat_profile == "Broomva Book Agent Turbo":
+        settings['model'] = "gpt-4-1106-preview"
 
     chain = RetrievalQAWithSourcesChain.from_chain_type(
         ChatOpenAI(temperature=settings['temperature'], streaming=settings['streaming'], model=settings['model']),
         chain_type="stuff",
         retriever=vector_store.as_retriever(search_kwargs={"k": int(settings['k'])}),
+        
     )
 
+    cl.user_session.set("settings", settings)
+    
+    print(settings)
     cl.user_session.set("chain", chain)
 
 
 @cl.on_message
 async def main(message):
     chain = cl.user_session.get("chain")  # type: RetrievalQAWithSourcesChain
+    
     cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+        stream_final_answer=True, #answer_prefix_tokens=["FINAL", "ANSWER"]
     )
     cb.answer_reached = True
 
@@ -132,3 +171,23 @@ async def main(message):
         await cl.Message(
             content=answer,
         ).send()
+
+# Instantiate the LLM
+llm = HuggingFaceHub(
+    model_kwargs={"max_length": 500},
+    repo_id="Broomva/bart-large-translation-spa-guc",
+)
+
+# Add the LLM provider
+add_llm_provider(
+    LangchainGenericProvider(
+        # It is important that the id of the provider matches the _llm_type
+        id=llm._llm_type,
+        # The name is not important. It will be displayed in the UI.
+        name="Spa - Guc Translation",
+        # This should always be a Langchain llm instance (correctly configured)
+        llm=llm,
+        # If the LLM works with messages, set this to True
+        is_chat=True
+    )
+)
--- chainlit.md
+++ chainlit.md
@@ -1,11 +1,8 @@
 # Welcome to the Broomva Book Chat
 
 This is the Q&A agent for the Broomva Book. It is a chat interface that uses retrieval to
-answer questions leveraging information found in the book.
+answer questions leveraging information found in the book. Go ahead and ask things like:
 
-Go ahead and ask things like:
-
-`What is the book about?`
 `What is machine learning and deep learning?`
+
 `what is quantum computing?`
-`can you explain the meaning of life?`
